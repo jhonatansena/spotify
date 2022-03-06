54f123d7c0cc05d888c7ac36c95c07c4c07aece9
Jhonatan da Costa Sena <jcostasenna@hotmail.com>
2022-03-06 18:36:59 -0300

    feat: add user and auth routes

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
+const cors = require("cors");
 const connection = require("./db");
+const userRoutes = require("./routes/users");
 const app = express();
+
 connection();
-const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, console.log(`Server is listen on port ${port}...`));
---
+++ routes/users.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const { User, validate } = require("../models/user");
+const bcrypt = require("bcrypt");
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send({ message: error.details[0].message });
+
+  const user = await User.findOne({ email: req.body.email });
+
+  if (user)
+    return res
+      .status(403)
+      .send({ message: "User with given email already exist!" });
+
+  const salt = await bcrypt.genSalt(Number(process.env.SALT));
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+  let newUser = await new User({
+    ...req.body,
+    password: hashPassword,
+  }).save();
+
+  newUser.password = undefined;
+  newUser._v = undefined;
+
+  res
+    .status(200)
+    .send({ data: newUser, message: "Account created successfully" });
+});
+
+module.exports = router;
