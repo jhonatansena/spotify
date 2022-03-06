a15efd975bb17466dd2bb724f971df5b763e4739
Jhonatan da Costa Sena <jcostasenna@hotmail.com>
2022-03-06 15:34:49 -0300

    infra initial

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const connection = require("./db");
+const app = express();
+connection();
+const port = process.env.PORT || 3000;
+
+app.listen(port, console.log(`Server is listen on port ${port}...`));
